4c51cdb9a5bb53494662d3ffec85ff7c0beb398e
rudeh2926 <dokyungkim2926@gmail.com>
2023-10-07 01:26:29 +0900

    🥲

---
+++ fox_kt/src/test/kotlin/com/example/fox_kt/FoxKtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.fox_kt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FoxKtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
