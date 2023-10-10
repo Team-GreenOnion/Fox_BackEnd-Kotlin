ba42152c41b59d8e38fa41eaacac8803a0d4a20d
rudeh2926 <dokyungkim2926@gmail.com>
2023-10-10 21:50:14 +0900

    내 정보 확인

--- src/main/kotlin/com/example/fox_kt/domain/user/presentation/UserController.kt
+++ src/main/kotlin/com/example/fox_kt/domain/user/presentation/UserController.kt
@@ -4,6 +4,7 @@ import com.example.fox_kt.domain.user.presentation.dto.request.FindPasswordWthEm
 import com.example.fox_kt.domain.user.presentation.dto.request.ModifyPasswordRequest
 import com.example.fox_kt.domain.user.presentation.dto.request.UserLoginRequest
 import com.example.fox_kt.domain.user.presentation.dto.request.UserSignupRequest
+import com.example.fox_kt.domain.user.presentation.dto.response.QueryMyInfoResponse
 import com.example.fox_kt.domain.user.service.*
 import com.example.fox_kt.global.security.jwt.dto.TokenResponse
 import org.springframework.web.bind.annotation.*
@@ -16,7 +17,8 @@ class UserController(
     private val userLoginService: UserLoginService,
     private val uploadUserProfileService: UploadUserProfileService,
     private val modifyPasswordService: ModifyPasswordService,
-    private val findPasswordWithEmailService: FindPasswordWithEmailService
+    private val findPasswordWithEmailService: FindPasswordWithEmailService,
+    private val queryMyInfoService: QueryMyInfoService
 ) {
     @PostMapping("/signup")
     fun signup(@RequestBody userSignupRequest: UserSignupRequest) =
@@ -37,4 +39,8 @@ class UserController(
     @PatchMapping("/new/password")
     fun passwordWithEmail(findPasswordWthEmailRequest: FindPasswordWthEmailRequest) =
         findPasswordWithEmailService.passwordWithEmail(findPasswordWthEmailRequest)
+
+    @GetMapping("/info")
+    fun queryMyInfo() : QueryMyInfoResponse =
+            queryMyInfoService.queryMyInfo()
 }
---
+++ src/main/kotlin/com/example/fox_kt/domain/user/presentation/dto/response/QueryMyInfoResponse.kt
@@ -0,0 +1,9 @@
+package com.example.fox_kt.domain.user.presentation.dto.response
+
+import com.example.fox_kt.domain.user.enums.Sex
+
+data class QueryMyInfoResponse (
+        val name: String,
+        val email: String,
+        val sex: Sex,
+)
