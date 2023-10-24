6bb2411d4690d31f4ede3cb98952354ead359ee8
rudeh2926 <dokyungkim2926@gmail.com>
2023-10-25 08:17:44 +0900

    가독성을 위해 Repository어노테이션 추가

--- src/main/kotlin/com/example/fox_kt/domain/school_open_chat/domain/repository/OpenChatJoinerRepository.kt
+++ src/main/kotlin/com/example/fox_kt/domain/school_open_chat/domain/repository/OpenChatJoinerRepository.kt
@@ -2,6 +2,8 @@ package com.example.fox_kt.domain.school_open_chat.domain.repository
 
 import com.example.fox_kt.domain.school_open_chat.domain.OpenChatJoiner
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface OpenChatJoinerRepository: JpaRepository<OpenChatJoiner, Long> {
 }
--- src/main/kotlin/com/example/fox_kt/domain/school_open_chat/domain/repository/OpenChatRoomRepository.kt
+++ src/main/kotlin/com/example/fox_kt/domain/school_open_chat/domain/repository/OpenChatRoomRepository.kt
@@ -2,6 +2,8 @@ package com.example.fox_kt.domain.school_open_chat.domain.repository
 
 import com.example.fox_kt.domain.school_open_chat.domain.OpenChatRoom
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface OpenChatRoomRepository: JpaRepository<OpenChatRoom, Long> {
 }
