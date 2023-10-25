df76ca61860d7ab8965802c8fa4e3b4788457bb1
rudeh2926 <dokyungkim2926@gmail.com>
2023-10-25 20:22:25 +0900

    삭제

--- src/main/kotlin/com/example/fox_kt/domain/school_open_chat/presentation/request/SendMessageRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.example.fox_kt.domain.school_open_chat.presentation.request
-
-data class SendMessageRequest (
-
-    val message: String? = null,
-
-    val openChatRoom: Long? = null
-)
