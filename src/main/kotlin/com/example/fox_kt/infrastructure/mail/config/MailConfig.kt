3e92f1df3734d1304839833edd5f55b2977fa67d
rudeh2926 <dokyungkim2926@gmail.com>
2023-10-26 10:32:46 +0900

    환경변수

--- src/main/kotlin/com/example/fox_kt/infrastructure/mail/config/MailConfig.kt
+++ src/main/kotlin/com/example/fox_kt/infrastructure/mail/config/MailConfig.kt
@@ -1,5 +1,6 @@
 package com.example.fox_kt.infrastructure.mail.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.mail.javamail.JavaMailSender
@@ -7,15 +8,22 @@ import org.springframework.mail.javamail.JavaMailSenderImpl
 
 
 @Configuration
-class EmailConfig {
+class EmailConfig (
+    @Value("spring.mail.username")
+    private val email: String,
+
+    @Value("spring.mail.password")
+    private val password: String
+
+){
 
     @Bean
     fun javaMailSender(): JavaMailSender {
         val mailSender = JavaMailSenderImpl()
         mailSender.host = "smtp.gmail.com"
         mailSender.port = 587
-        mailSender.username = "dokyungkim2926@dsm.hs.kr"
-        mailSender.password = "ptlrknwogsfgaldz"
+        mailSender.username = email
+        mailSender.password = password
         val props = mailSender.javaMailProperties
         props["mail.transport.protocol"] = "smtp"
         props["mail.smtp.auth"] = "true"
