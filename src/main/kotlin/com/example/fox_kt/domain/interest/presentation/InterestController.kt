d955f0f63221643874b33723473cd227c7c3290f
rudeh2926 <dokyungkim2926@gmail.com>
2023-10-28 21:14:30 +0900

    스웨거 interest추가

--- src/main/kotlin/com/example/fox_kt/domain/interest/presentation/InterestController.kt
+++ src/main/kotlin/com/example/fox_kt/domain/interest/presentation/InterestController.kt
@@ -3,6 +3,7 @@ package com.example.fox_kt.domain.interest.presentation
 import com.example.fox_kt.domain.interest.presentation.dto.request.InterestRequest
 import com.example.fox_kt.domain.interest.service.InterestService
 import com.example.fox_kt.domain.interest.service.ModifyInterestService
+import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PatchMapping
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "interest", description = "관심분야 선택")
 @RestController
 @RequestMapping("/interest")
 class InterestController(
@@ -18,11 +20,13 @@ class InterestController(
     private val modifyInterestService : ModifyInterestService
 ) {
 
+    @Operation(summary = "관심분야 선택")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     fun selectInterest(interestRequest: InterestRequest) =
         interestService.selectInterest(interestRequest)
 
+    @Operation(summary = "관심분야 수정")
     @PatchMapping
     fun modifyInterest(interestRequest: InterestRequest) =
          modifyInterestService.modifyInterest(interestRequest)
