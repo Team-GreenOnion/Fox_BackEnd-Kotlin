d800037419a09da37ab2430651f99c61912406fa
rudeh2926 <dokyungkim2926@gmail.com>
2023-10-28 21:17:02 +0900

    email swagger 추가

--- src/main/kotlin/com/example/fox_kt/infrastructure/mail/presentation/EmailController.kt
+++ src/main/kotlin/com/example/fox_kt/infrastructure/mail/presentation/EmailController.kt
@@ -1,6 +1,7 @@
 package com.example.fox_kt.infrastructure.mail.presentation
 
 import com.example.fox_kt.infrastructure.mail.service.CreateEmailCodeService
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController
 class EmailController(
     private val createEmailCodeService: CreateEmailCodeService,
 ) {
+
+    @Operation(summary = "이메일 인증번호 생성")
     @PostMapping
     fun requestEmailCode(@RequestParam email : String) : String? =
          createEmailCodeService.sendVerificationCode(email)
