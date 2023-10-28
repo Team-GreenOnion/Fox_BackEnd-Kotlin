15f98da4e5a895ab6af26f6abf77253cbf00595a
rudeh2926 <dokyungkim2926@gmail.com>
2023-10-28 21:15:05 +0900

    스웨거 openChat 생성

--- src/main/kotlin/com/example/fox_kt/domain/school_open_chat/presentation/OpenChatController.kt
+++ src/main/kotlin/com/example/fox_kt/domain/school_open_chat/presentation/OpenChatController.kt
@@ -2,6 +2,7 @@ package com.example.fox_kt.domain.school_open_chat.presentation
 
 import com.example.fox_kt.domain.school_open_chat.presentation.request.CreateOpenChatRequest
 import com.example.fox_kt.domain.school_open_chat.service.CreateOpenChatService
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -15,6 +16,7 @@ class OpenChatController(
     private val createOpenChatService: CreateOpenChatService
 ) {
 
+    @Operation(summary = "오픈채팅방 생성")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     fun createOpenChat(@RequestBody createOpenChatRequest: CreateOpenChatRequest) =
