b5648be019cc81c5c8466a3caaba7fed91f20c24
rudeh2926 <dokyungkim2926@gmail.com>
2023-10-29 21:00:23 +0900

    비동기 제거

--- src/main/kotlin/com/example/fox_kt/infrastructure/mail/service/CreateEmailCodeService.kt
+++ src/main/kotlin/com/example/fox_kt/infrastructure/mail/service/CreateEmailCodeService.kt
@@ -15,31 +15,26 @@ class CreateEmailCodeService (
     private val mailSender: JavaMailSender,
     private val redisTemplate: StringRedisTemplate
 ){
-    @Async
-    fun sendVerificationCode(email: String): String {
+    fun generateVerificationCode(): String {
         val characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
         val random = SecureRandom()
         val codeBuilder = StringBuilder(6)
-
         for (i in 0..5) {
             val index = random.nextInt(characters.length)
             codeBuilder.append(characters[index])
         }
-
-        val verificationCode = codeBuilder.toString()
-
-        val message = SimpleMailMessage().apply {
-            setTo(email)
-            subject = "회원 가입 인증 코드"
-            text = "인증 코드: $verificationCode"
-        }
-
-        mailSender.send(message)
-
-        redisTemplate.opsForValue().set(email, verificationCode)
-        redisTemplate.expire(email, 6, TimeUnit.MINUTES)
-
-        return verificationCode
+        return codeBuilder.toString()
     }
 
+    fun sendVerificationCode(email: String): String {
+        val verificationCode = generateVerificationCode()
+        val message = SimpleMailMessage()
+        message.setTo(email)
+        message.subject = "회원 가입 인증 코드"
+        message.text = "인증 코드: $verificationCode"
+        mailSender.send(message)
+        redisTemplate.opsForValue()[email] = verificationCode
+        redisTemplate.expire(email, 6, TimeUnit.MINUTES)
+        return verificationCode
+    }
 }
