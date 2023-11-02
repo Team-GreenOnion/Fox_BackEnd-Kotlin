6f83b466057e1043b100567c6bb82d263d0c199d
도경 <dokyungkim2926@gmail.com>
2023-11-02 16:27:40 +0900

    if문 string redis로 변경

--- src/main/kotlin/com/example/fox_kt/domain/user/service/UserSignupService.kt
+++ src/main/kotlin/com/example/fox_kt/domain/user/service/UserSignupService.kt
@@ -3,10 +3,11 @@ package com.example.fox_kt.domain.user.service
 import com.example.fox_kt.domain.user.domain.User
 import com.example.fox_kt.domain.user.domain.repository.UserRepository
 import com.example.fox_kt.domain.user.exception.EmailAlreadyExistsException
+import com.example.fox_kt.domain.user.exception.EmailCodeMissMatchException
 import com.example.fox_kt.domain.user.exception.NameAlreadyExistsException
 import com.example.fox_kt.domain.user.exception.PasswordMissMatchException
 import com.example.fox_kt.domain.user.presentation.dto.request.UserSignupRequest
-import com.example.fox_kt.infrastructure.mail.domain.repository.MailRepository
+import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -15,10 +16,14 @@ import org.springframework.transaction.annotation.Transactional
 class UserSignupService (
     private val userRepository: UserRepository,
     private val passwordEncoder: PasswordEncoder,
+    private val stringRedisTemplate: StringRedisTemplate
 ){
     @Transactional
     fun signup (userSignupRequest: UserSignupRequest) {
 
+        if (userSignupRequest.validEmailCode != stringRedisTemplate.opsForValue().get(userSignupRequest.email)) {
+            throw EmailCodeMissMatchException
+        }
 
         if (userRepository.existsByEmail(userSignupRequest.email)) {
             throw EmailAlreadyExistsException
