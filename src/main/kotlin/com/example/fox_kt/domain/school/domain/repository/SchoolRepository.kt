7f7665a24ed0a0427bf841876ad4cd3642450ef9
rudeh2926 <dokyungkim2926@gmail.com>
2023-11-06 22:23:04 +0900

    삭제

--- src/main/kotlin/com/example/fox_kt/domain/school/domain/repository/SchoolRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.example.fox_kt.domain.school.domain.repository
-
-import com.example.fox_kt.domain.school.domain.School
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface SchoolRepository : JpaRepository<School, Long>{
-}
