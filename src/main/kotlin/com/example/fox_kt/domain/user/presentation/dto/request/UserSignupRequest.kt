288150ae3042a10cd48096043fca8b2be109e982
도경 <dokyungkim2926@gmail.com>
2023-11-10 10:31:45 +0900

    Notnull처리

--- src/main/kotlin/com/example/fox_kt/domain/user/presentation/dto/request/UserSignupRequest.kt
+++ src/main/kotlin/com/example/fox_kt/domain/user/presentation/dto/request/UserSignupRequest.kt
@@ -1,13 +1,20 @@
 package com.example.fox_kt.domain.user.presentation.dto.request
 
 import com.example.fox_kt.domain.user.enums.Sex
+import org.jetbrains.annotations.NotNull
 
 
 data class UserSignupRequest(
+    @NotNull
     val email :String,
+    @NotNull
     val password : String,
+    @NotNull
     val validPassword : String,
+    @NotNull
     val validEmailCode : String,
+    @NotNull
     val name : String,
+    @NotNull
     val sex : Sex
 )
