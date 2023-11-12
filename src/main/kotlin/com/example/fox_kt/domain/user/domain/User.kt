8aabffd2831b54820138b332ad44d612d096d704
도경 <dokyungkim2926@gmail.com>
2023-11-12 15:04:25 +0900

    user entity Column추가

--- src/main/kotlin/com/example/fox_kt/domain/user/domain/User.kt
+++ src/main/kotlin/com/example/fox_kt/domain/user/domain/User.kt
@@ -3,6 +3,7 @@ package com.example.fox_kt.domain.user.domain
 import com.example.fox_kt.domain.interest.domain.Interest
 import com.example.fox_kt.domain.user.enums.Sex
 import com.example.fox_kt.global.entity.BaseEntity
+import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.EnumType
 import javax.persistence.Enumerated
@@ -11,12 +12,22 @@ import javax.persistence.OneToMany
 @Entity
 class User(
     id : Long?,
+
+    @Column(name = "email", nullable = false)
     val email:String,
+
+    @Column(name = "password", nullable = false)
     var password: String,
+
+    @Column(name = "name", nullable = false)
     val name: String,
+
+    @Column(name = "profile_image_url", nullable = false)
     var profileUrl: String? = null,
+
     @Enumerated(EnumType.STRING)
     val sex: Sex,
+    
     @OneToMany
     val interest: List<Interest>? = null
 ) : BaseEntity(id) {
