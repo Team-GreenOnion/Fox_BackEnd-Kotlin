5740f907b7f4b75bc4b4975ace38ed078b4cbba2
도경 <dokyungkim2926@gmail.com>
2023-11-24 21:26:11 +0900

    해결

--- src/main/kotlin/com/example/fox_kt/domain/interest/presentation/InterestController.kt
+++ src/main/kotlin/com/example/fox_kt/domain/interest/presentation/InterestController.kt
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
@@ -23,12 +24,12 @@ class InterestController(
     @Operation(summary = "관심분야 선택")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    fun selectInterest(interestRequest: InterestRequest) =
+    fun selectInterest(@RequestBody interestRequest: InterestRequest) =
         interestService.selectInterest(interestRequest)
 
     @Operation(summary = "관심분야 수정")
     @PatchMapping
-    fun modifyInterest(interestRequest: InterestRequest) =
+    fun modifyInterest(@RequestBody interestRequest: InterestRequest) =
          modifyInterestService.modifyInterest(interestRequest)
 
 }
