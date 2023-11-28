88c4a55b6b842041f818778a31dd20c03d947f0c
도경 <dokyungkim2926@gmail.com>
2023-11-28 17:09:22 +0900

    쓸데없는 improt 제거

--- src/main/kotlin/com/example/fox_kt/domain/interest/service/ModifyInterestService.kt
+++ src/main/kotlin/com/example/fox_kt/domain/interest/service/ModifyInterestService.kt
@@ -1,7 +1,6 @@
 package com.example.fox_kt.domain.interest.service
 
 import com.example.fox_kt.domain.interest.domain.Interest
-import com.example.fox_kt.domain.interest.domain.repository.InterestRepository
 import com.example.fox_kt.domain.interest.presentation.dto.request.InterestRequest
 import com.example.fox_kt.domain.user.domain.User
 import com.example.fox_kt.domain.user.facade.UserFacade
@@ -10,7 +9,6 @@ import org.springframework.transaction.annotation.Transactional
 
 @Service
 class ModifyInterestService(
-    private val interestRepository: InterestRepository,
     private val userFacade: UserFacade
 ) {
     @Transactional
