b1b95af922a554804f996bddfd191aa39b7bfd65
도경 <dokyungkim2926@gmail.com>
2023-12-06 11:02:40 +0900

    adsf

--- src/main/kotlin/com/example/fox_kt/domain/open_chat/presentation/ChatController.kt
+++ src/main/kotlin/com/example/fox_kt/domain/open_chat/presentation/ChatController.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/chat")
+@RequestMapping("/open/chat")
 class ChatController(
     private val createOpenChatRoomService: CreateOpenChatRoomService
 ) {
--- src/main/kotlin/com/example/fox_kt/global/security/jwt/dto/TokenResponse.kt
+++ src/main/kotlin/com/example/fox_kt/global/security/jwt/dto/TokenResponse.kt
@@ -3,5 +3,4 @@ package com.example.fox_kt.global.security.jwt.dto
 class TokenResponse(
     val accessToken: String,
     val refreshToken: String,
-    val expiredAt: Long
 )
