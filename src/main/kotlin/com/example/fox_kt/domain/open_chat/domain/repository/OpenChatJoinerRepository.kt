5987748f8c35b41a26c3a6ac9251011d665cb4ec
도경 <dokyungkim2926@gmail.com>
2023-12-06 23:07:12 +0900

    asdf

--- src/main/kotlin/com/example/fox_kt/domain/open_chat/domain/repository/OpenChatJoinerRepository.kt
+++ src/main/kotlin/com/example/fox_kt/domain/open_chat/domain/repository/OpenChatJoinerRepository.kt
@@ -1,6 +1,5 @@
 package com.example.fox_kt.domain.open_chat.domain.repository
 
-import com.example.fox_kt.domain.open_chat.domain.OpenChat
 import com.example.fox_kt.domain.open_chat.domain.OpenChatJoiner
 import com.example.fox_kt.domain.open_chat.domain.OpenChatRoom
 import com.example.fox_kt.domain.user.domain.User
